4b76a9cdc6ed51deb0843d6223aa2485dd38e42d
Minna Win <minnawin@ucar.edu>
2015-09-01 20:12:34 -0600

    Implemented changes to accomodate the forcing configurations

--- scripts/Python/WRF_Hydro_forcing.py
+++ scripts/Python/WRF_Hydro_forcing.py
@@ -56,6 +56,7 @@ def regrid_data( product_name, parser ):
     # scripts.
     product = product_name.upper()
     dst_grid_name = parser.get('regridding','dst_grid_name')
+    ncl_exec = parser.get('exe', 'ncl_exe')
 
     # Create an array to store the elapsed times for
     # regridding each file.
@@ -124,7 +125,7 @@ def regrid_data( product_name, parser ):
         input_filename =  data_file_to_process
         srcfilename_param =  "'srcfilename=" + '"' + input_filename +  \
                              '"' + "' "
-        #logging.info("input data file: %s", data_file_to_process)
+        logging.info("input data file: %s", data_file_to_process)
         wgtFileName_in_param =  "'wgtFileName_in = " + '"' + wgt_file + \
                                 '"' + "' "
         dstGridName_param =  "'dstGridName=" + '"' + dst_grid_name + '"' + "' "
@@ -141,7 +142,7 @@ def regrid_data( product_name, parser ):
         if product == "HRRR" or product == "NAM" \
            or product == "GFS" or product == "RAP":
            full_output_file = output_file_dir + "/"  + subdir_file_path
-           logging.info("Full output filename for %s: %s", product, full_output_file)
+           #logging.info("Full output filename for %s: %s", product, full_output_file)
            # Create the new output file subdirectory
            mkdir_p(output_file_dir)
            outFile_param = "'outFile=" + '"' + hydro_filename+ '"' + "' "
@@ -152,7 +153,7 @@ def regrid_data( product_name, parser ):
            # accept an outdir variable.  Incorporate the output directory (outdir)
            # into the outFile variable.
            full_output_file = output_file_dir + "/"  + hydro_filename
-           logging.info("Full output filename for %s: %s", product, full_output_file)
+           #logging.info("Full output filename for %s: %s", product, full_output_file)
            mkdir_p(output_file_dir)
            outFile_param = "'outFile=" + '"' + full_output_file + '"' + "' "
    
@@ -162,7 +163,7 @@ def regrid_data( product_name, parser ):
         regrid_prod_cmd = ncl_exec + " "  + regrid_params + " " + \
                           regridding_exec
         
-        logging.debug(regrid_prod_cmd)
+        #logging.debug(regrid_prod_cmd)
         # Measure how long it takes to run the NCL script for regridding.
         start_NCL_regridding = time.time()
         return_value = os.system(regrid_prod_cmd)
@@ -288,7 +289,7 @@ def create_output_name_and_subdir(product, filename, input_data_file):
     # Assemble the filename and the full output directory path
     hydro_filename = year_month_day + "_" + init_hr + \
                       "_" + fcst_hr + "_" + product_name + ".nc"
-    logging.debug("Generated the output filename for %s: %s",product, hydro_filename)
+    #logging.debug("Generated the output filename for %s: %s",product, hydro_filename)
 
     return (year_month_day_subdir, hydro_filename)
 
@@ -306,7 +307,7 @@ def mkdir_p(dir):
                  exist.
 
     """
-    logging.info("Directory to be made: %s",dir)
+    #logging.info("Directory to be made: %s",dir)
 #    if not os.path.exists(os.path.dirname(dir)):
 #       logging.info("inside mkdir_p...creating directory: %s", dir)
 #       os.makedirs(os.path.dirname(dir))
@@ -320,21 +321,26 @@ def mkdir_p(dir):
         else: raise            
 
 
-def downscale_data(product_name, parser):
+def downscale_data(product_name, parser, shortwave=False):
     """
-    Performs downscaling of HRRR and NAM data.  
-    NOTE: MRMS data DOES NOT need to be downscaled.      
+    Performs downscaling of data by calling the necessary
+    NCL or Fortran code.  
 
 
     Args:
-        product_name (string):  The product name: HRRR or NAM. MRMS data does 
-                           not need to be downscaled.
+        product_name (string):  The product name: ie HRRR, NAM, GFS, etc. 
 
         parser (ConfigParser) : The ConfigParser which can access the
                                 Python config file wrf_hydro_forcing.parm
                                 and retrieve the file names and locations
                                 of input.
 
+        shortwave (boolean) :   True if downscaling of shortwave 
+                                radiation (SWDOWN) is necessary, in which
+                                case invoke the NCL wrapper to the Fortan
+                                code that performs the downscaling.
+                                False by default, don't downscale
+                                the shortwave radiation.
     Returns:
         elapsed_array (List):  A list of the elapsed time for
                                performing the downscaling. Each entry 
@@ -349,9 +355,8 @@ def downscale_data(product_name, parser):
     """
 
     # Read in all the relevant input parameters based on the product: 
-    # HRRR or NAM.
+    # HRRR, NAM, GFS, etc.
     product = product_name.upper() 
-    ncl_exe = parser.get('exe', 'ncl_exe')
     lapse_rate_file = parser.get('downscaling','lapse_rate_file')
  
  
@@ -359,6 +364,7 @@ def downscale_data(product_name, parser):
     # downscaling each file.
     elapsed_array = []
     downscale_exe = parser.get('exe', 'downscaling_exe')
+    ncl_exec = parser.get('exe', 'ncl_exe')
 
     if product  == 'HRRR':
         logging.info("Downscaling HRRR")
@@ -378,6 +384,11 @@ def downscale_data(product_name, parser):
         hgt_data_file = parser.get('downscaling','GFS_hgt_data')
         geo_data_file = parser.get('downscaling','GFS_geo_data')
         downscale_output_dir = parser.get('downscaling', 'GFS_downscale_output_dir')
+
+        # Retrieve the executable for downscaling SWDOWN, shortwave radiation.
+        if shortwave:
+            downscale_swdown_exe = parser.get('downscaling', 'RAP_downscaling_fortran_exe') 
+
     elif product == 'RAP':
         logging.info("Downscaling RAP")
         data_to_downscale_dir = parser.get('downscaling','RAP_data_to_downscale')
@@ -424,15 +435,21 @@ def downscale_data(product_name, parser):
                             input_file3_param + lapse_file_param + \
                             output_file_param 
         downscale_cmd = ncl_exe + " " + downscale_params + " " + downscale_exe
-        logging.debug("Downscale command: %s", downscale_cmd)
+
+        # Key-value pairs for downscaling SWDOWN, shortwave radiation.
+        swdown_output_file_param = "'outFile=" + '"' + full_output_filename + '"' + "' "
+        swdown_geo_file_param = "'inputGeo=" + '"' + geo_data_file + '"' + "' "
+        swdown_params = swdown_geo_file_param + " " + swdown_output_file_param
+        downscale_shortwave = ncl_exe + " " + swdown_params + " " + downscale_swdown_exe 
+        #logging.debug("Downscale command: %s", downscale_cmd)
         start = time.time()
         return_value = os.system(downscale_cmd)
+        swdown_return_value = os.system(swdown_downscale_cmd)
         end = time.time()
         elapsed = end - start
         elapsed_array.append(elapsed)
-        #logging.info("*Benchmark* Time to regrid %s= %s secs", product_name, elapsed)
     
-        if return_value != 0:
+        if return_value != 0 or swdown_return_value != 0:
             logging.info('ERROR: The downscaling of %s was unsuccessful, \
                           return value of %s', product,return_value)
        
@@ -484,52 +501,6 @@ def create_benchmark_summary(product, activity, elapsed_times):
 #--------------------Define the Workflow -------------------------
 
 if __name__ == "__main__":
-    # Parse the config/parm file 
-    parser = SafeConfigParser()
-    parser.read('wrf_hydro_forcing.parm')
-    
-    # Identify which product to process: HRRR, MRMS, GFS, etc.
-    product = "GFS"   
-
-    # From the config/parm file, get any relevant executables...
-    ncl_exec = parser.get('exe', 'ncl_exe')
-    ncarg_root = parser.get('default_env_vars','ncarg_root')
- 
-    # From the config/parm file, retrieve the Logging level
-    # Set the logging to the appropriate level, DEBUG for more verbose, 
-    # or CRITICAL for the least verbose.  If no logging level is set, then
-    # set logging to a minimum of CRITICAL.  
-    logging_level = parser.get('log_level', 'forcing_engine_log_level')
-    if logging_level == 'DEBUG':
-        set_level = logging.DEBUG
-    elif logging_level == 'INFO':
-        set_level = logging.INFO
-    elif logging_level == 'WARNING':
-        set_level = logging.WARNING
-    elif logging_level == 'ERROR':
-        set_level = logging.ERROR
-    else: 
-        #if logging_level == CRITICAL / default case
-        set_level = logging.CRITICAL
- 
- 
-    # Configure the log file format to include a timestamp
-    logging_filename = product + "_forcing.log" 
-    logging.basicConfig(format='%(asctime)s %(message)s', 
-                        filename=logging_filename, level=set_level)
-     
- 
-    # Check for the NCARG_ROOT environment variable. If it is not set,
-    # use an appropriate default, defined in the configuration file.
-    ncarg_root_found = os.getenv("NCARG_ROOT")
-    if ncarg_root_found is None:
-        ncarg_root = os.environ["NCARG_ROOT"] = ncarg_root
-    #elapsed_time_array = regrid_data(product,parser)
-    #create_benchmark_summary(product,"Regridded", elapsed_time_array)
-    elapsed_time_array = downscale_data(product,parser)
-    logging.info("elapsed time array size: %s", len(elapsed_time_array)
-    create_benchmark_summary(product,"Downscaled", elapsed_time_array)
-
-    
-
-
+    # Replace pass with anything you wish if you want to
+    # run this as a standalone program.
+    pass
