fd8bd84871a88fe8f7c9bf57d2d3ca14270301c9
Minna Win <minnawin@ucar.edu>
2015-10-15 14:35:24 -0600

    initial check in of the Long Range Forcing config: has stub for bias correction

---
+++ scripts/Python/Long_Range_Forcing.py
@@ -0,0 +1,54 @@
+import WRF-Hydro_forcing as whf
+import logging
+import os
+from ConfigParser import SafeConfigParser
+
+#----------------------------------------------
+
+if __name__ == "__main__":
+    parser = SafeConfigParser()
+    parser.read('wrf_hydro_forcing.parm')
+
+    ncl_exec = parser.get('exe', 'ncl_exe')
+    ncarg_root = parser.get('default_env_vars', 'ncarg_root')
+    logging_level = parser.get('log_level', 'forcing_engine_log_level')
+    log_file_dir = parser.get('log_level', 'output_log_directory')
+    forcing_config_label = "Long-Range"
+
+    # Check for the NCARG_ROOT environment variable. If it is not set,
+    # use an appropriate default, defined in the configuration file.
+    ncarg_root_found = os.getenv("NCARG_ROOT")
+    if ncarg_root_found is None:
+        ncarg_root = os.environ["NCARG_ROOT"] = ncarg_root
+    
+    # Set the logging level based on what was defined in the parm/config file
+    if logging_level == 'DEBUG':
+        set_level = logging.DEBUG
+    elif logging_level == 'INFO':
+        set_level = logging.INFO
+    elif logging_level == 'WARNING':
+        set_level = logging.WARNING
+    elif logging_level == 'ERROR':
+        set_level = logging.ERROR
+    else:
+        set_level = logging.CRITICAL
+
+    logging_filename = forcing_config_label + "_forcing.log" 
+    logging.basicConfig(format='%(asctime)s %(message)s',
+                         filename=logging_filename, level=set_level)
+
+    # Bias correct the CFS data
+    # TO BE IMPLEMENTED 
+    # CFS_bias_corrections = whf.bias_corr("CFS", parser)
+
+    # Regrid the CFS data.
+    CFS_regrids = whf.regrid_data("CFS",parser)     
+    
+    # Downscale the CFS data.
+    CFS_downscalings = whr.downscale_data("CFS", parser)
+   
+    # Generate the metrics for regridding and downscaling and write to the
+    # logfile, which by default is saved to the directory from which this
+    # script in invoked.
+    whf.create_benchmark("CFS","Regridding", CFS_regrids)
+    whf.create_benchmark("CFS","Downscaling", CFS_downscalings)
