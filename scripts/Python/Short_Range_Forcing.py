d44de83e8c947da4df64b1310a9cbf049450d2ee
Minna Win <minnawin@hydro-c1.rap.ucar.edu>
2015-11-04 22:56:16 +0000

    Refactor in progress- support for workflow manager, triggering forcing based on data-driven event, and limit processing to forecast range appropriate for the data product and forcing configureation

--- scripts/Python/Short_Range_Forcing.py
+++ scripts/Python/Short_Range_Forcing.py
@@ -1,59 +1,96 @@
-import Test as whf
+import WRF_Hydro_forcing as whf
 import logging
 import os
+import sys
 from ConfigParser import SafeConfigParser
-#import WRF-Hydro_forcing
+#
 
-#----------------------------------------------
+"""Short_Range_Forcing
+Performs regridding,downscaling, bias
+correction (if needed), and layering/mixing 
+of data products associated with the Short Range
+forcing configuration.  Invokes methods in the
+WRF_Hydro_forcing module and input parameters
+that are defined in the wrf_hydro_forcing.parm
+parameter/configuration file.  Logs to a log
+file that is created in the same directory
+from where this script is executed.
 
-if __name__ == "__main__":
+"""
+
+
+def main():
+    # Initialize anything that is specific to this
+    # forcing configuration...
+    forcing_config_label = 'Short_Range'
+
+    # Read the parameters from the config/param file.
     parser = SafeConfigParser()
-    parser.read('test.parm')
+    parser.read('wrf_hydro_forcing.parm')
 
-    ncl_exec = parser.get('exe', 'ncl_exe')
-    ncarg_root = parser.get('default_env_vars', 'ncarg_root')
-    logging_level = parser.get('log_level', 'forcing_engine_log_level')
-    log_file_dir = parser.get('log_level', 'output_log_directory')
-    forcing_config_label = "Analysis_Assimilation"
+    # Set up logging, environments, etc.
+    forcing_config_label = "Short_Range"
+    logging = whf.initial_setup(parser,forcing_config_label)
 
-    # Check for the NCARG_ROOT environment variable. If it is not set,
-    # use an appropriate default, defined in the configuration file.
-    ncarg_root_found = os.getenv("NCARG_ROOT")
-    if ncarg_root_found is None:
-        ncarg_root = os.environ["NCARG_ROOT"] = ncarg_root
-    
-    # Set the logging level based on what was defined in the parm/config file
-    if logging_level == 'DEBUG':
-        set_level = logging.DEBUG
-    elif logging_level == 'INFO':
-        set_level = logging.INFO
-    elif logging_level == 'WARNING':
-        set_level = logging.WARNING
-    elif logging_level == 'ERROR':
-        set_level = logging.ERROR
-    else:
-        set_level = logging.CRITICAL
-
-    logging_filename = forcing_config_label + "_forcing.log" 
-    logging.basicConfig(format='%(asctime)s %(message)s',
-                         filename=logging_filename, level=set_level)
-
-    # Regrid the RAP, NAM, and HRRR data.
-    RAP_regrids = whf.regrid_data("RAP",parser, ncl_exec)     
-    NAM_regrids = whf.regrid_data("NAM", parser, ncl_exec)
-    HRRR_regrids = whf.regrid_data("HRRR", parser, ncl_exec)
-    
-    # Downscale the RAP, NAM and HRRR data.
-    RAP_downscalings = whr.downscale_data("RAP", parser, ncl_exec)
-    NAM_downscalings = whf.downscale_data("NAM", parser, ncl_exec)
-    HRRR_downscalings = whf.downscale_data("HRRR", parser, ncl_exec)
+    # Retrieve the information from the input args
+    args = whf.read_input()
+    curr_data_file = args.InputFileName
+    product_data_name = args.DataProductName
    
-    # Generate the metrics for regridding and downscaling and write to the
-    # logfile, which by default is saved to the directory from which this
-    # script in invoked.
-    whf.create_benchmark("RAP","Regridding", RAP_regrids)
-    whf.create_benchmark("RAP","Downscaling", RAP_downscalings)
-    whf.create_benchmark("NAM","Regridding", NAM_regrids)
-    whf.create_benchmark("NAM","Downscaling", NAM_downscalings)
-    whf.create_benchmark("HRRR","Regridding", HRRR_regrids)
-    whf.create_benchmark("HRRR","Downscaling", HRRR_downscalings)
+    # Extract the date, model run time, and forecast hour from the file name
+    # Use the fcsthr to process only the files that have a fcst hour less than
+    # the max fcst hr defined in the param/config file.
+    (date,modelrun,fcsthr) = whf.extract_file_info(curr_data_file)
+    
+    # Determine whether this current file lies within the forecast range
+    # for the data product (e.g. if processing RAP, use only the 0hr-18hr forecasts).
+    # Skip if this file has a forecast hour greater than the max indicated in the 
+    # parm/config file.
+    in_fcst_range = whf.is_in_fcst_range(product_data_name, fcsthr, parser)
+
+    if in_fcst_range:
+        # Check for RAP or GFS data products.  If this file is
+        # a 0 hr fcst and is RAP or GFS, substitute each 0hr forecast
+        # with the file from the previous model run and the same valid
+        # time.  This is necessary because there are missing variables
+        # in the 0hr forecasts (e.g. precip rate for RAP and radiation
+        # in GFS).
+
+        print "within fcst range"
+        if args.regrid_downscale:
+            logging.info("Regridding and Downscaling for %s", args.InputFileName)
+            # Determine if this is a 0hr forecast for RAP data (GFS is also missing
+            # some variables for 0hr forecast, but GFS is not used for Short Range
+            # forcing). We will need to substitute this file for the downscaled
+            # file from a previous model run with the same valid time.  
+            # We only need to do this for downscaled files, as the Short Range 
+            # forcing files that are regridded always get downscaled and we don't want
+            # to do this for both the regridding and downscaling.
+            if fcsthr == 0 and product_data_name == 'RAP':
+                logging.info("Regridding, ignoring f0 RAP files " )
+                regridded_file = whf.regrid_data(product_data_name, curr_data_file, parser, True)
+                whf.downscale_data(product_data_name,regridded_file, parser,True,True)                
+            else:
+                regridded_file = whf.regrid_data(product_data_name, curr_data_file, parser)
+                whf.downscale_data(product_data_name,regridded_file, parser,True)                
+            
+        elif args.layer:
+            logging.info("Layering requested for %s", args.InputFileName)
+     
+        elif args.bias:
+            logging.info("Bias correction requested for %s", args.InputFileName)
+
+    else:
+        # Skip processing this file, exiting...
+        sys.exit(1)
+
+ 
+    
+    
+#--------------------------    
+    
+   
+if __name__ == "__main__":
+    main()
+    
+
