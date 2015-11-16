15846620bdf74284d2788562285c792c4208e62e
Minna Win <minnawin@hydro-c1.rap.ucar.edu>
2015-11-16 23:27:54 +0000

    Support for use via cli and as an imported module (stand-alone and module)

--- scripts/Python/Short_Range_Forcing.py
+++ scripts/Python/Short_Range_Forcing.py
@@ -17,9 +17,133 @@ file that is created in the same directory
 from where this script is executed.
 
 """
+def forcing(action, prod, file, prod2=None, file2=None):
+    """Peforms the action on the given data
+       product and corresponding input file.
+
+       Args:
+           action (string):  Supported actions are:
+                             'regrid' - regrid and downscale
+                             'bias'   - bias correction 
+                                        (requires two 
+                                        products and two files)
+                             'layer'  - layer (requires two
+                                        products and two files)
+           prod (string):  The first product [mandatory option]:
+                            (HRRR or RAP)
+           file (string):  The file name (full path not necessary,
+                            this is derived from the Python config/
+                            param file and the YYYMMDD portion of 
+                            the file name.
+
+          prod2 (string):   The second product (RAP or HRRR), default
+                            is None. Required for layering.
+          file2 (string):   The second file name, required for 
+                            layering, default is None.
+       Returns:
+           None           Performs the indicated action on the
+                          files based on the type of product and
+                          any other relevant information provided
+                          by the Python config/param file,
+                          wrf_hydro_forcing.parm
+ 
+ 
+    """
 
 
-def main():
+    # Initialize anything that is specific to this
+    # forcing configuration...
+    forcing_config_label = 'Short_Range'
+
+    # Read the parameters from the config/param file.
+    parser = SafeConfigParser()
+    parser.read('wrf_hydro_forcing.parm')
+
+    # Set up logging, environments, etc.
+    forcing_config_label = "Short_Range"
+    logging = whf.initial_setup(parser,forcing_config_label)
+
+
+    # Extract the date, model run time, and forecast hour from the file name
+    # Use the fcsthr to process only the files that have a fcst hour less than
+    # the max fcst hr defined in the param/config file.
+    (date,modelrun,fcsthr) = whf.extract_file_info(file)
+    
+    # Convert the action to lower case 
+    # and the product name to upper case
+    # for consistent checking
+    action_requested = action.lower()
+    product_data_name = prod.upper()
+
+    # Determine whether this current file lies within the forecast range
+    # for the data product (e.g. if processing RAP, use only the 0hr-18hr forecasts).
+    # Skip if this file has a forecast hour greater than the max indicated in the 
+    # parm/config file.
+    in_fcst_range = whf.is_in_fcst_range(prod, fcsthr, parser)
+
+    if in_fcst_range:
+        # Check for RAP or GFS data products.  If this file is
+        # a 0 hr fcst and is RAP or GFS, substitute each 0hr forecast
+        # with the file from the previous model run and the same valid
+        # time.  This is necessary because there are missing variables
+        # in the 0hr forecasts (e.g. precip rate for RAP and radiation
+        # in GFS).
+
+        if action_requested == 'regrid':
+            logging.info("Regridding and Downscaling for %s", product_data_name)
+            # Determine if this is a 0hr forecast for RAP data (GFS is also missing
+            # some variables for 0hr forecast, but GFS is not used for Short Range
+            # forcing). We will need to substitute this file for the downscaled
+            # file from a previous model run with the same valid time.  
+            # We only need to do this for downscaled files, as the Short Range 
+            # forcing files that are regridded always get downscaled and we don't want
+            # to do this for both the regridding and downscaling.
+            if fcsthr == 0 and prod == 'RAP':
+                logging.info("Regridding, ignoring f0 RAP files " )
+                regridded_file = whf.regrid_data(product_data_name, file, parser, True)
+                whf.downscale_data(product_data_name,regridded_file, parser, True, True)                
+            else:
+                regridded_file = whf.regrid_data(product_data_name, file, parser, False)
+                whf.downscale_data(product_data_name,regridded_file, parser,True, False)                
+            
+        elif action_requested == 'layer':
+            logging.info("Layering requested for %s and %s", prod, prod2)
+            if prod2 is None:
+               logger.error("ERROR [Short_Range_Forcing]: layering requires two products")
+            elif file2 is None:
+               logger.error("ERROR [Short_Range_Forcing]: layering requires two input files")
+             
+        elif action_requested == 'bias':
+            
+            logging.info("Bias correction requested for %s", file)
+            if prod2 is None:
+                logger.error("ERROR [Short_Range_Forcing]: bias correction requires two products")
+            elif file2 is None:
+                logger.error("ERROR [Short_Range_Forcing]: bias correction requires two input files")
+
+    else:
+        # Skip processing this file, exiting...
+        logging.info("INFO [Short_Range_Forcing]- Skip processing, requested file is outside max fcst")
+        return
+
+
+
+def cli():
+    """The command line version for running
+       the Short Range Forcing. Useful for testing.
+
+       Args:
+           None   args and options are provided in the
+                  command line and from the Python config/
+                  param file.
+
+       Returns:
+           None   performs the requested action on the
+                  specified product(s) and file(s).
+       
+    """
+
+
     # Initialize anything that is specific to this
     # forcing configuration...
     forcing_config_label = 'Short_Range'
@@ -134,6 +258,6 @@ def main():
     
    
 if __name__ == "__main__":
-    main()
+    cli()
     
 
